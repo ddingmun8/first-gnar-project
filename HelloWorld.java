905266e923b0ce89c8fd18c5ec6d6f0e66a8dba4
Gnar <ddingmun8@gmail.com>
2022-04-03 12:52:54 +0900

    gnar 2 commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hi~");
         System.out.println("Hi~ Hello");
+        System.out.println("Hi~ Hello");
     }
     
 }
